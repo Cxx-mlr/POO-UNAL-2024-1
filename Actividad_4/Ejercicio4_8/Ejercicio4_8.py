b1c125973cbff1a4dd9c34965bcec98d52851aaf
Cxx-mlr <spapiernik12@gmail.com>
2024-08-19 17:14:12 -0500

    Refactored Ejercicio4_8 to improve readability by using keyword arguments when instantiating objects.

--- Actividad_4/Ejercicio4_8/Ejercicio4_8.py
+++ Actividad_4/Ejercicio4_8/Ejercicio4_8.py
@@ -6,10 +6,30 @@ from CyclingRace.TimeTrialist import TimeTrialist
 class Ejercicio4_8:
 	@staticmethod
 	def main():
-		team = Team("Sky", "Estados Unidos")
-		sprinter = Sprinter(123979, "Geraint Thomas", 320.0, 25.0)
-		climber = Climber(123980, "Egan Bernal", 25.0, 10.0)
-		time_trialist = TimeTrialist(123981, "Jonathan Castroviejo", 120.0)
+		team = Team(
+			name="Sky",
+			country="Estados Unidos"
+		)
+
+		sprinter = Sprinter(
+			identifier=123979,
+			name="Geraint Thomas",
+			average_power=320.0,
+			average_speed=25.0
+		)
+
+		climber = Climber(
+			identifier=123980,
+			name="Egan Bernal",
+			average_acceleration=25.0,
+			ramp_grade=10.0
+		)
+
+		time_trialist = TimeTrialist(
+			identifier=123981,
+			name="Jonathan Castroviejo",
+			maximum_speed=120.0
+		)
 
 		team.add_cyclist(sprinter)
 		team.add_cyclist(climber)
